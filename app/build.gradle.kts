eeb1cb504103a77a8b238f767e021ff4ec54e4f5
qisbilqissabrin <rayn2320@gmail.com>
2025-09-23 20:52:34 +0700

    Tugas 2 Pertemuan 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
 }
 
 android {
+    viewBinding {
+        enable = true
+    }
     namespace = "com.example.ifunsoed"
     compileSdk = 36
 
@@ -42,6 +45,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.constraintlayout)
+    implementation(libs.google.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
